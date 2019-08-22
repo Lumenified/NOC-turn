3b03521b8c909de8bc839c4c4995efccf9a39152
Lumenified <tahsinsaid.turan@gazi.edu.tr>
2019-08-23 00:34:20 +0300

    Gaussian Distribution with particles

--- NocExamples/GaussianDistribution/sketch.js
+++ NocExamples/GaussianDistribution/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(640, 360);
+	background(255);
 }
 
 function draw() {
+	let xloc = randomGaussian();
+	let sd = 60;
+	let mean = width/2;
+	xloc = (xloc * sd) + mean;
 
+	fill(0,10);
+	noStroke();
+	ellipse(xloc, height/2,16,16);
 }
